85e7a18c45d71c067b9a25e3a00f6f7e7e573849
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-04-07 11:55:28 -0400

    Adds server side data validation for New User Register page

--- a4-shankar-ghimire/register.php
+++ a4-shankar-ghimire/register.php
@@ -17,40 +17,88 @@ Course: ...............
         <?php
         require_once 'header.php';
         ?>
+           <?php
+            //to display the user back to the user
+           $new_user_name = "";
+           $username_error="";
+           $password_error="";
+           $confirm_password_error = "";
+           $record_insert_message ="";
+            if (isset($_REQUEST['result'])) {
+                if ($_REQUEST['result'] == "userexists") {
+                    $record_insert_message = "This User name is already taken up. Please try again";
+                    //echo "<p> This User name is already taken up. Please try again.</p>";
+                } else if ($_REQUEST['result'] == "success") {
+                    $record_insert_message = "New user was registered.";
+                    //echo '<p>New user was registered</p>';
+                } else if ($_REQUEST['result'] == "fail") {
+                    $record_insert_message = "New user was not registered.<br> Something went wrong.<br> Try again, please.";
+                    //echo "<p>New user was not registered</p>";
+                }else if($_REQUEST['result'] == "validation_fail"){
+                    $new_user_name= $_SESSION['new_user_name'];
+                    $username_error = $_SESSION['username_error'];
+                    $password_error = $_SESSION['password_error'];
+                    $confirm_password_error = $_SESSION['confirm_password_error'];
+                    unset($_SESSION['new_user_name']);
+                    unset($_SESSION['username_error']);
+                    unset($_SESSION['password_error']);
+                    unset($_SESSION['confirm_password_error']);
+                }
+            }
+            
+            ?>
         <div class="wrapper">
 
             <div class="home-main-wrapper">
                 
                 <h2>New User Register</h2>
+                <?php
+//                echo "<br>user name error". $username_error;
+//            echo "<br>password error" . $password_error;
+//            echo "<br> confirm password error ". $confirm_password_error;
+                ?>
                 <form id='userregistration' action="register1.php" method="post">
                     <table class='register-table'>
                         <tr class="register-row">
                             <td>Username:</td>
-                            <td><input id='username' type="text" name="username" autofocus><span id='username-error' class='error-message'></span></td>
+                            <td>
+                                <input id='username' type="text" name="username" value="<?=$new_user_name; ?>" autofocus>
+                                <span id='usernameerror' class='error-message'><?php echo $username_error; ?></span>
+                            </td>
                         </tr>
                         <tr>
                             <td>Password:</td>
-                            <td><input id='password' type="password" name="password"><span id='password-error' class='error-message'></td>
+                            <td>
+                                <input id='password' type="password" name="password">
+                                <span id='passworderror' class='error-message'><?php echo $password_error; ?></span>
+                            </td>
                         </tr>
                         <tr>
                             <td>Confirm Password:</td>
-                            <td><input id='confirmpassword' type="password" name="confirmpassword"><span id='confirmpassword-error' class='error-message'></td>
+                            <td>
+                                <input id='confirmpassword' type="password" name="confirmpassword">
+                                <span id='confirmpassworderror' class='error-message'><?php echo $confirm_password_error; ?></span>
+                            </td>
                         </tr>
                         <tr ><td colspan="2" style="text-align: center;"><input type="submit" value="Register"></td></tr>
+                        <tr>
+                            <td colspan="2" style="font-weight: bold;">
+                                <?php
+                                    echo $record_insert_message;
+                                ?>
+                            </td> 
+                        </tr>
+                        
+                        
                     </table>
                 </form>
             </div>
             <?php
-            //to display the user back to the user
-            if (isset($_REQUEST['result'])) {
-                if ($_REQUEST['result'] == "userexists") {
-                    echo "<p> This User name is already taken up. Please try again.</p>";
-                } else if ($_REQUEST['result'] == "success") {
-                    echo '<p>New user was registered</p>';
-                } else if ($_REQUEST['result'] == "fail") {
-                    echo "<p>New user was not registered</p>";
-                }
-            }
+                //clears the variables after printing
+                $new_user_name = "";
+                //$username_error = "";
+                //$password_error = "";
+                //$confirm_password_error = "";
             ?>
             <br><br><br><br><br><br><br>
             <br><br><br><br><br><br><br>
--- a4-shankar-ghimire/register1.php
+++ a4-shankar-ghimire/register1.php
@@ -1,35 +1,78 @@
 <?php
-//echo "Testing";
+    session_start();
     ob_start();//to avoid 'header's already sent ...' warning
     require_once 'config.php';
-//    echo "testing";
+    //echo "testing";
     
     //extracts user credential from register.php
     $username=$_REQUEST['username'];
     $password=$_REQUEST['password'];
     $confirm_password=$_REQUEST['confirmpassword'];
+    //testing
+//    echo "user name is: ". $username;
+//    echo "password is : " . $password;
+//    echo "confirm password is : ". $confirm_password;
     
     $is_valid="ok";
-    
-    //encrypts the password
-    $hashed_password = password_hash($_REQUEST['password'],PASSWORD_DEFAULT);
-       
-    //checks if the username alreadys exists or not
-    $query = "select username from tblusers where username='$username';";
-    $result = mysqli_query($conn, $query);
-    //if a row is fetched, then username exists
-    if(mysqli_num_rows($result)){
-        //redirects to register.php page
-        header('location:register.php?result=userexists');
-    }else{
-        
-        //inserts user info into the tblusers in database
-        $query = "insert into tblusers(username,password) values ('". $username ."','". $hashed_password ."');";
-        $result = mysqli_query($conn, $query);
-        if($result == 1){
-            header('location:register.php?result=success');
-        }else{
-            header('location:register.php?result:fail');
-        }
+    //data validation
+    if(empty($username)){
+        $_SESSION['username_error']="UserName cannot be blank!";
+        $error_msg="error-empty-username";
+        $is_valid="no";
+        //echo $is_valid;
+    }else if(strlen($username) < 5) {
+        $_SESSION['username_error']="UserName must be at least 5 characters long!";
+        $error_msg="error-min-length-username";
+        $is_valid="no";
+//        echo $is_valid;
+    }else if(empty($password)){
+        $_SESSION['password_error']="Password cannot be blank!";
+        $_SESSION['new_user_name']= $username;
+        $error_msg="error-empty-password";
+        $is_valid="no";
+    }else if(strlen($password)< 5) {
+        $_SESSION['new_user_name']= $username;
+        $_SESSION['username_error']="Password must be at least 5 characters long!";
+        $error_msg="error-minlength-password";
+        $is_valid="no";
     }
+    else if( strcmp($password,$confirm_password)!=0){
+        $_SESSION['new_user_name']= $username;
+        $_SESSION['confirm_password_error']="Confirm Password does not match!";
+        $error_msg="error-confirm-password";
+        $is_valid="no";
+    }
+    //echo $error_msg;//for testing
+    //echo "<br>".$is_valid;//for testing
+    if($is_valid=="ok"){
+        //echo $is_valid;//testing purpose
+        //code to inserts into database
+        //encrypts the password
+        $hashed_password = password_hash($_REQUEST['password'],PASSWORD_DEFAULT);
+
+        //checks if the username alreadys exists or not
+        $query = "select username from tblusers where username='$username';";
+        $result = mysqli_query($conn, $query);
+        //if a row is fetched, then username exists
+        if(mysqli_num_rows($result)){
+            //redirects to register.php page
+            header('location:register.php?result=userexists');
+        }else{
+
+            //inserts user info into the tblusers in database
+            $query = "insert into tblusers(username,password) values ('". $username ."','". $hashed_password ."');";
+            $result = mysqli_query($conn, $query);
+            if($result == 1){
+                //echo "record inserted into table.";//testing purpose
+                header('location:register.php?result=success');
+            }else{
+                header('location:register.php?result=fail');
+            }
+    }      
+    }else{
+        //echo $error_msg;//testing purpose
+        //redirects to the register page
+        header('location:register.php?result=validation_fail');
+    }
+    
 ?>
